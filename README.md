548cf6a220e96fd94f09d5268d99b548d674af02
2or3 <t.nagashima.cog@gmail.com>
2014-01-30 21:14:01 +0900

    fix setup.rb

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # set up
+#### First. Install VirtualBox Manager 4.3.6 and Vagrant 1.4.3.
+- VirtualBox official site.
+ - https://www.virtualbox.org/
+- Vagrant official site.
+ - http://www.vagrantup.com/
+- Please read the following link.
+ - http://blog.collarks.net/?p=76
+
+#### Second. Execute following script.
 ```
 $ ruby setup.rb
+```
+#### Third. Execute following command for start vm.
+```
+$ cd ~/Vagrant/debian7
+$ vagrant up
+```
+###### Login vm.
+```
+$ vagrant ssh
 ```
--- setup.rb
+++ setup.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/ruby
-exec("vagrant box add debian7.3.0 http://open.collarks.com/test/debian7.3.0-wheezly.box")
-exec("mkdir -p ~/Vagrant/debian7")
-exec("cd ~/Vagrant/debian7")
-exec("vagrant init debian7.3.0")
+print "Add vagrant box debian7.3.0\n"
+res = system("vagrant box add debian7.3.0 http://open.collarks.com/test/debian7.3.0-wheezly.box")
+if res then
+    print "Start vagrant init debian7.3.0\n"
+    system("mkdir -p ~/Vagrant/debian7")
+    system("vagrant init debian7.3.0")
+    system("mv Vagrantfile ~/Vagrant/debian7")
+else
+    print "Command failed\n"
+    print "Aborted!!\n"
+end
